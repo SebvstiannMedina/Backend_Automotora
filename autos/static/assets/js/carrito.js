970cdbae9d75992979b2abdfc18291e33a4edca6
SebvstiannMedina <seba.medina@duocuc.cl>
2024-07-01 11:14:31 -0400

    Listo

--- autos/static/assets/js/carrito.js
+++ autos/static/assets/js/carrito.js
@@ -107,7 +107,7 @@ function mostrarElementosLista() {
             <div>
                 <div class="img">
                     <button onclick="eliminar(${i})" class="botonTrash">
-                        <img src="../assets/img/trash.png" alt="Eliminar">
+                        <img src="../../../static/assets/img/trash.png" alt="Eliminar">
                     </button>
                     <p>${lista[i].nombre}</p>
                 </div>
